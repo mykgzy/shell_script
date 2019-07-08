def68e24802959686932a9f96952b1dc97e4b0e1
mykgzy <mykgzy@gmail.com>
2019-07-08 16:28:21 +0800

    添加 numactl 判断以及通知消息

--- nic_balance.sh
+++ nic_balance.sh
@@ -2,26 +2,47 @@
 # nic_balance.sh
 # usage: nic_balance.sh NIC_NAME
 
-# 获取 node1 节点上的 CPU ID,组合成数组
-cpu=(`numactl --hardware | grep "node 1 cpus" | awk -F: '{print $2}'`)
-len_cpu=${#cpu[@]}
+# 消息通知字段
+function msg ()
+{
+    echo "$(date +%F) $(date +%T) | $1"
+    return 0
+}
 
-# 获取某网卡接口上的中断 ID，并组合成数组
-nic_irq=(`grep $1 /proc/interrupts|awk '{print $1}'|sed 's/://'`)
-len_nic_irq=${#nic_irq[@]}
+# 主函数定义
+function main()
+{
+	# 获取 node1 节点上的 CPU ID,组合成数组
+	cpu=(`numactl --hardware | grep "node 1 cpus" | awk -F: '{print $2}'`)
+	len_cpu=${#cpu[@]}
 
-# 将 node1 上的 CPU 与对应网卡上的 ID 进行绑定
-for ((n=0; n<$len_nic_irq; n++))
-do
+	# 获取某网卡接口上的中断 ID，并组合成数组
+	nic_irq=(`grep $1 /proc/interrupts|awk '{print $1}'|sed 's/://'`)
+	len_nic_irq=${#nic_irq[@]}
 
-    if [ $n -lt $len_cpu ]
-    then
-        echo ${cpu[n]} > /proc/irq/${nic_irq[n]}/smp_affinity_list
-        echo "echo ${cpu[n]} > /proc/irq/${nic_irq[n]}/smp_affinity_list"
-    else
-        m=`expr $n - $len_cpu`
-        echo ${cpu[m]} > /proc/irq/${nic_irq[n]}/smp_affinity_list
-        echo "echo ${cpu[m]} > /proc/irq/${nic_irq[n]}/smp_affinity_list"
-    fi
+	# 将 node1 上的 CPU 与对应网卡上的 ID 进行绑定
+	for ((n=0; n<$len_nic_irq; n++))
+	do
+	    if [ $n -lt $len_cpu ]
+	    then
+	        echo ${cpu[n]} > /proc/irq/${nic_irq[n]}/smp_affinity_list
+	        echo "echo ${cpu[n]} > /proc/irq/${nic_irq[n]}/smp_affinity_list"
+	    else
+	        m=`expr $n - $len_cpu`
+	        echo ${cpu[m]} > /proc/irq/${nic_irq[n]}/smp_affinity_list
+	        echo "echo ${cpu[m]} > /proc/irq/${nic_irq[n]}/smp_affinity_list"
+	    fi
+	done
+}
 
-done
+
+# 判断是否安装 numactl
+numactl -H >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+	main
+else
+	msg "Please check numactl module installed or not"
+	exit 1
+fi
+
